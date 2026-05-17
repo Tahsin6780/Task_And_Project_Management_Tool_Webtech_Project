21a5ef552c3256ef503702c7466c5f5108f3f4dc
RisalatSanjan <skrisalat.sanjan@gmail.com>
2026-05-18 02:45:54 +0600

    Add unarchive and delete buttons to archived view

--- view/project_archived.php
+++ view/project_archived.php
@@ -14,22 +14,43 @@
 <div class="container">
 	<h1>Archived Projects</h1>
 
+	<div id="ajax-message"></div>
+
 	<?php if (empty($projects)): ?>
-		<div class="card">
+		<div class="card" id="empty-state">
 			<p>No archived projects.</p>
 		</div>
 	<?php endif; ?>
 
-	<div class="project-grid">
+	<div class="project-grid" id="project-grid">
 		<?php foreach ($projects as $project): ?>
-			<div class="card project-card" style="border-left-color: <?= e($project['color_label']) ?>">
+			<div class="card project-card" id="project-card-<?= e($project['id']) ?>" style="border-left-color: <?= e($project['color_label']) ?>">
 				<h2><?= e($project['name']) ?></h2>
 				<p><?= e($project['description']) ?></p>
 				<p><strong>Status:</strong> Archived</p>
+
+				<div class="actions">
+					<button
+						type="button"
+						class="unarchive-btn"
+						data-project-id="<?= e($project['id']) ?>"
+					>
+						Unarchive
+					</button>
+
+					<button
+						type="button"
+						class="delete-btn"
+						data-project-id="<?= e($project['id']) ?>"
+					>
+						Delete
+					</button>
+				</div>
 			</div>
 		<?php endforeach; ?>
 	</div>
 </div>
 
+<script src="controller/projects.js"></script>
 </body>
 </html>

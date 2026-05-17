0e3bafe199e36208f67fd1d627bb2ad50a909a7c
[jarintasnim3123] <[jarintasnim01315@gmail.com]>
2026-05-17 15:55:24 +0600

    updated

--- View/register.php
+++ View/register.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
 
+// Redirect already-logged-in users
+if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']) {
+    header("Location: navbar.php");
+    exit();
+}
+
+
 $nameError = $_SESSION["nameError"] ?? "";
 $emailError = $_SESSION["emailError"] ?? "";
 $passwordError = $_SESSION["passwordError"] ?? "";
 
 $name = $_SESSION["name"] ?? "";
 $email = $_SESSION["email"] ?? "";
-
-$isLoggedIn = $_SESSION["isLoggedIn"] ?? false;
-
-// if($isLoggedIn){
-//     header("Location: dashboard.php");
-//     exit();
-// }
 
 unset($_SESSION["nameError"]);
 unset($_SESSION["emailError"]);
@@ -39,64 +39,48 @@ unset($_SESSION["email"]);
         <table>
 
             <tr>
-                <td>Name</td>
+            <td>Name</td>
+            <td>
+                <input type="text" name="name" value="<?php echo htmlspecialchars($name); ?>">
+            </td>
+            <td style="color:red;"><?php echo $nameError; ?></td>
+        </tr>
 
-                <td>
-                    <input type="text" name="name"
-                    value="<?php echo $name; ?>">
-                </td>
+        <tr>
+            <td>Email</td>
+            <td>
+                <input type="text" name="email" value="<?php echo htmlspecialchars($email); ?>">
+            </td>
+            <td style="color:red;"><?php echo $emailError; ?></td>
+        </tr>
 
-                <td style="color:red;">
-                    <?php echo $nameError; ?>
-                </td>
-            </tr>
+        <tr>
+            <td>Password</td>
+            <td>
+                <input type="password" name="password">
+            </td>
+            <td style="color:red;"><?php echo $passwordError; ?></td>
+        </tr>
 
-            <tr>
-                <td>Email</td>
+        <tr>
+            <td></td>
+            <td>
+                <!-- name="register" so AuthController can tell this from login -->
+                <input type="submit" name="register" value="Register">
+            </td>
+        </tr>
 
-                <td>
-                    <input type="text" name="email"
-                    value="<?php echo $email; ?>">
-                </td>
+        <tr>
+            <td></td>
+            <td>
+                Already have an account?
+                <a href="login.php">Login</a>
+            </td>
+        </tr>
 
-                <td style="color:red;">
-                    <?php echo $emailError; ?>
-                </td>
-            </tr>
+    </table>
 
-            <tr>
-                <td>Password</td>
-
-                <td>
-                    <input type="password" name="password">
-                </td>
-
-                <td style="color:red;">
-                    <?php echo $passwordError; ?>
-                </td>
-            </tr>
-
-            <tr>
-                <td></td>
-
-                <td>
-                    <input type="submit" name="register" value="Register">
-                </td>
-            </tr>
-
-            <tr>
-                <td></td>
-
-                <td>
-                    Already have an account?
-                    <a href="login.php">Login</a>
-                </td>
-            </tr>
-
-        </table>
-
-    </form>
+</form>
 
 </body>
-
 </html>
